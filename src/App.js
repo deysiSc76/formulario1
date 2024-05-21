602766db7f2032752be8b71c918a8ef7b23c5665
deysiSc76 <deysi.santacruz76@gmail.com>
2024-05-21 19:52:44 -0400

    Formulario1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Form from './component/Form';
+import Result from './component/Result';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    firsName:"",
+    lastName:"",
+    email:"",
+    password:"",
+    confirmPassword:""
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Form inputs={state} setInputs={setState}/>
+    <Result data={state}/>
     </div>
   );
 }
